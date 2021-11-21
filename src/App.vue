6566ef1ed029af97f08117262b2a960f2955d8fc
Elham Ahmadzadeh <eliahm63@gmail.com>
2021-11-21 19:47:10 +0100

    json-server added

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,99 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Header
+      @toggle-add-task="toggleAddTask"
+      title="Task Tracker"
+      :showAddTask="showAddTask"
+    />
+    <div v-show="showAddTask">
+      <AddTask @add-task="addTask" />
+    </div>
+
+    <Tasks
+      @toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+      :tasks="tasks"
+    />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from "./components/Header.vue";
+import Tasks from "./components/Tasks.vue";
+import AddTask from "./components/AddTask.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+    Tasks,
+    AddTask,
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "hjhbjhv jhjhvhv",
+        day: "nmnnbjh tgcgd",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "hjhbjhv jhjhvhv",
+        day: "nmnnbjh tgcgd",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "hjhbjhv jhjhvhv",
+        day: "nmnnbjh tgcgd",
+        reminder: false,
+      },
+    ];
+  },
+  data() {
+    return {
+      tasks: [],
+      showAddTask: true,
+    };
+  },
+  methods: {
+    addTask(task) {
+      this.tasks = [...this.tasks, task];
+    },
+    deleteTask(id) {
+      if (confirm("are you sure ?")) {
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+      }
+    },
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask;
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      );
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: "Poppins", sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
 }
 </style>
